e90775137e163fca88db023a33d3549dff6525eb
patriciagonzalez <patrcgd@gmail.com>
2016-12-14 11:09:00 -0200

    primeiro commit
    How to cemitool

---
+++ cemitool/how_to_cemitool.R
@@ -0,0 +1,3 @@
+# This is how to run Cemitool
+
+system("Rscript CEMiTool.R collapsed_symbols.txt -i Matheus_dbInteractions.txt -o Pati_results -t template.txt -s ReactomePathways.gmt -f TRUE --samples-column geo_accession")
