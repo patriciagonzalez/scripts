dd6cc415677e0f053412955e063f13757bb74175
patriciagonzalez <patrcgd@gmail.com>
2016-12-14 11:10:18 -0200

    Update how_to_cemitool.R

--- cemitool/how_to_cemitool.R
+++ cemitool/how_to_cemitool.R
@@ -1,3 +1,3 @@
-# This is how to run Cemitool
-
+# This is how to run cemitool
+#pedro
 system("Rscript CEMiTool.R collapsed_symbols.txt -i Matheus_dbInteractions.txt -o Pati_results -t template.txt -s ReactomePathways.gmt -f TRUE --samples-column geo_accession")
